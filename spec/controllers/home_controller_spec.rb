b7e72e8de832c51ed02338018f953da69167849c
Matthew Ahern <matt.r.ahern@gmail.com>
2017-04-22 19:45:38 -0500

    worked out initial devise setup

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -4,10 +4,22 @@ RSpec.describe HomeController, type: :controller do
   render_views
 
   describe "GET /index" do
+    
     it "has 'Welcome to SkillEx' on the page" do
       get :index
 
       expect(response.body).to include("Welcome to SkillEx")
     end
+
+    it "shows a Register and Login button" do
+      get :index
+
+      expect(response.body).to include("Login")
+      expect(response.body).to include("Register")
+    end
+
   end
+
+
+
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,3 +1,5 @@
+require 'devise'
+require 'spec_helper'
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
@@ -54,4 +56,7 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include Devise::Test::ControllerHelpers, type: :view
 end
