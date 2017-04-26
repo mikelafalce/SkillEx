1d46ccabcccacf543206fbbd040db703da177858
Michael Lee <mleenavy@gmail.com>
2017-04-26 14:48:07 -0500

    enabled students to request lessons through skill show page, view requests on home page as teacher, and confirm those lessons through lesson show page

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,6 +1,6 @@
 class LessonsController < ApplicationController
-  before_action :set_lesson, only: [:show, :edit, :update, :destroy]
-
+  before_action :set_lesson, only: [:show, :edit, :update, :destroy, :confirm]
+  before_action :authenticate_user!
   
   def index
     @lessons = Lesson.all
@@ -15,18 +15,30 @@ class LessonsController < ApplicationController
 
   def new
     @lesson = Lesson.new
+    @skill = Skill.find_by(id: params[:skill])
   end
 
   
-  def edit
+  def confirm
+    if @lesson.teacher != current_user
+      redirect_to @lesson, notice: 'You can only confirm lessons you teach.'
+    else 
+      @lesson.confirmed_at = DateTime.now 
+      @lesson.save
+      redirect_to @lesson, notice: 'Lesson was successfully confirmed.'
+    end
   end
 
    
   def create
     @lesson = Lesson.new(lesson_params)
+    @lesson.skill = Skill.find_by(id: params[:skill])
+    @lesson.teacher = @lesson.skill.teacher
+    @lesson.student = current_user
+    @lesson.requested_at = DateTime.now
     respond_to do |format|
       if @lesson.save
-        format.html { redirect_to @lesson, notice: 'Lesson was successfully created.' }
+        format.html { redirect_to @lesson, notice: 'Lesson was successfully requested.' }
         format.json { render :show, status: :created, location: @lesson }
       else
         format.html { render :new }
@@ -61,7 +73,7 @@ class LessonsController < ApplicationController
 
   private
     def set_lesson
-      @lesson = lesson.find(params[:id])
+      @lesson = Lesson.find(params[:id])
     end
 
     def lesson_params
--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -1,5 +1,59 @@
 class SkillsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :find_skill, only: [:show, :edit, :update, :destroy, :join, :leave]
+
   def index
-    @skills=Skill.all
+    @skills = Skill.all
+  end
+
+  def new
+    @skill = Skill.new
+  end
+
+  def create
+    @skill = Skill.new(skill_params)
+    @skill.teacher = current_user
+    if @skill.save
+      current_user.skills << @skill
+      redirect_to @skill, notice: 'Skill was successfully created.'
+    else
+      render :new, locals: { skill: @skill }
+    end
+  end
+
+  def show
+    @teacher = @skill.teacher
+  end
+
+  def edit
+  end
+
+  def update
+    if current_user != @skill.teacher
+      redirect_to @skill, notice: 'You can only edit your own skill.'
+    elsif @skill.update(skill_params)
+      redirect_to @skill, notice: 'Skill was successfully updated.'
+    else
+      render :edit, locals: { skill: @skill }
+    end
+  end
+
+  def destroy
+    if current_user != @skill.teacher
+      redirect_to @skill, notice: 'You can only delete your own skill.'
+    elsif @skill.destroy
+      redirect_to skills_path, notice: 'Skill was successfully deleted.'
+    end
+  end
+
+
+  private
+
+  def find_skill
+    @skill = Skill.find(params[:id])
+  end
+
+  def skill_params
+    params.require(:skill).permit(:title, :description)
   end
 end
