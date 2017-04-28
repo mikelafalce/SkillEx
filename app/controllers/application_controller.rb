b596ac77689bdecb5ae265b40bb00e502ae2b1d3
Michael Lee <mleenavy@gmail.com>
2017-04-28 16:22:01 -0500

    solved rspec failures

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,9 @@ class ApplicationController < ActionController::Base
 
 
   def need_rating
-      unrated_teacher_lessons = current_user.lessons_as_teacher.where(teacher_rating_student:nil).select {|l| l.start_time < DateTime.now}
-    if unrated_teacher_lessons.count > 0
+    if current_user
+      unrated_teacher_lessons = current_user.lessons_as_teacher.where(teacher_rating_student:nil).select {|l| l.start_time < DateTime.now if l.start_time}
+    elsif unrated_teacher_lessons && unrated_teacher_lessons.count > 0
       redirect_to lesson_rating_path(unrated_teacher_lessons.first)
 
       # prompt student for rating
