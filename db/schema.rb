f1bc63286dbd27c74f53014c2f37b1e15260b03f
Brian <bmoloney82@gmail.com>
2017-04-30 16:35:20 -0500

    location testing

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170429023223) do
+ActiveRecord::Schema.define(version: 20170430192717) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -69,6 +69,7 @@ ActiveRecord::Schema.define(version: 20170429023223) do
     t.string "last_name"
     t.string "avatar"
     t.integer "points", default: 5
+    t.string "location"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
