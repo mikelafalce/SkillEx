73b4c609979f4356f2790ced65aa8dea8dfc8248
mikelafalce <mikelafalce@gmail.com>
2017-05-01 09:02:50 -0500

    added parameter to create_users.rb migration

--- db/migrate/20170422233450_devise_create_users.rb
+++ db/migrate/20170422233450_devise_create_users.rb
@@ -11,7 +11,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.1]
 
       ## Rememberable
       t.datetime :remember_created_at
-
+      t.string :location
       ## Trackable
       t.integer  :sign_in_count, default: 0, null: false
       t.datetime :current_sign_in_at
