715defa6de00e70d09546dd4651a50555a49c314
Matthew Ahern <matt.r.ahern@gmail.com>
2017-05-03 18:57:50 -0500

    try 2 on heroku

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,32 +1,16 @@
 CarrierWave.configure do |config|
+  config.root = Rails.root.join('tmp') # adding these...
+  config.cache_dir = 'carrierwave' # ...two lines
+
   config.fog_credentials = {
-    # Configuration for Amazon S3 should be made available through an Environment variable.
-    # For local installations, export the env variable through the shell OR
-    # if using Passenger, set an Apache environment variable.
-    #
-    # In Heroku, follow http://devcenter.heroku.com/articles/config-vars
-    #
-    # $ heroku config:add S3_KEY=your_s3_access_key S3_SECRET=your_s3_secret S3_REGION=eu-west-1 S3_ASSET_URL=http://assets.example.com/ S3_BUCKET_NAME=s3_bucket/folder
-
-    # Configuration for Amazon S3
-    :provider              => 'AWS',
-    :aws_access_key_id     => 'AKIAJ47Q7LLUGB4RDA7A',
-    :aws_secret_access_key => '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',
-    :region                => 'us-east-2'
+    :provider               => 'AWS',                        # required
+    :aws_access_key_id      => 'AKIAJ47Q7LLUGB4RDA7A',                        # required
+    :aws_secret_access_key  => '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',                     # required
+    :region                 => 'us-east-2',                  # optional, defaults to 'us-east-1'
+    :host                   => 's3.amazonaws',             # optional, defaults to nil
+    :endpoint               => 'https://s3.amazonaws.com/skill-ex' # optional, defaults to nil
   }
-
-  # For testing, upload files to local `tmp` folder.
-  if Rails.env.test? || Rails.env.cucumber?
-    config.storage = :file
-    config.enable_processing = false
-    config.root = "#{Rails.root}/tmp"
-  else
-    config.storage = :fog
-  end
-
-  config.cache_dir = "#{Rails.root}/tmp/uploads"                  # To let CarrierWave work on heroku
-
-  config.fog_directory    = 'skill-ex'
-  config.s3_access_policy = :public_read                          # Generate http:// urls. Defaults to :authenticated_read (https://)
-  config.fog_host         = "s3.amazonaws.com/skill-ex/skill-ex"
+  config.fog_directory  = 'directory'                             # required
+  config.fog_public     = false                                   # optional, defaults to true
+  config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
 end
