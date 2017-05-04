a41fa9289102f3e6d93d74baceb22762dd97e501
Matthew Ahern <matt.r.ahern@gmail.com>
2017-05-04 17:01:13 -0500

    another heroku update

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,8 +2,8 @@ CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'                        # required
   config.fog_credentials = {
     provider:              'AWS',                        # required
-    aws_access_key_id:     ENV['AKIAJILDTWPHEA6XBYYA'],                        # required
-    aws_secret_access_key: ENV['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'],                        # required
+    aws_access_key_id:     ENV['AWS_ACCESS_KEY'],                        # required
+    aws_secret_access_key: ENV['AWS_SECRET_KEY'],                        # required
     region:                'us-east-2',                  # optional, defaults to 'us-east-1'
     host:                  's3-us-east-2.amazonaws.com',             # optional, defaults to nil
     endpoint:              'http://s3-us-east-2.amazonaws.com/skill-ex' # optional, defaults to nil
